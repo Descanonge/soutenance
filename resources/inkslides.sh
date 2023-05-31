ce03f38dafebd185e9d9514fa8f12c7158652c5c
Clément Haëck <52320542+Descanonge@users.noreply.github.com>
2023-05-31 19:37:16 +0200

    Add some multiple slide graphics

---
+++ resources/inkslides.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -eu
+
+infile=${1:-unset}
+
+if [[ "$infile" == "unset" ]]; then
+    echo "file must be input as argument"
+    exit 1
+fi
+
+if [[ ! -f "$infile" ]]; then
+    echo "file ${infile} does not exist"
+    exit 1
+fi
+
+name="$(basename "$infile" .svg)"
+basedir="$(dirname "$infile")"
+outdir="${basedir}/${name}"
+
+mkdir -p "${outdir}"
+cp "$infile" "${outdir}/"
+python "${basedir}/inkscape-slide.py" "${outdir}/${name}.svg" --format "${outdir}/{1:d}.pdf"
